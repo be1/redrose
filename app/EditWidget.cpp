5e47cd744c03b31e660fff1114f7a58ca9f54226
Benoît Rouits <brouits@free.fr>
2022-10-05 20:45:24 +0200

    Minor fix.

--- app/EditWidget.cpp
+++ app/EditWidget.cpp
@@ -3,7 +3,7 @@
 
 EditWidget::EditWidget(const QString& fileName, QWidget *parent)
     : QWidget(parent),
-      editvboxlayout(fileName, nullptr),
+      editvboxlayout(fileName),
       filename(fileName)
 {
 	setObjectName("EditWidget:" + fileName);
