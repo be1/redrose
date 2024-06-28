b3d6f7d2b001174aefb555e30e5fe3045bfcb398
Benoît Rouits <brouits@free.fr>
2024-06-28 11:35:55 +0200

    Fix compilation on Qt 6.2.4

--- app/ScoreMenu.cpp
+++ app/ScoreMenu.cpp
@@ -18,7 +18,7 @@ ScoreMenu::ScoreMenu(QWidget* parent)
 {
     setTitle(tr("Score"));
 
-#if (QT_VERSION < QT_VERSION_CHECK(6,0,0))
+#if (QT_VERSION < QT_VERSION_CHECK(6, 3, 0))
     addAction(tr("New"), this, &ScoreMenu::onNewActionTriggered, QKeySequence::New);
     addAction(tr("Open"), this, &ScoreMenu::onOpenActionTriggered, QKeySequence::Open);
 #else
@@ -38,7 +38,7 @@ ScoreMenu::ScoreMenu(QWidget* parent)
 
     updateRecentFileActions();
 
-#if (QT_VERSION < QT_VERSION_CHECK(6,0,0))
+#if (QT_VERSION < QT_VERSION_CHECK(6, 3, 0))
     addAction(tr("Save"), this, &ScoreMenu::onSaveActionTriggered, QKeySequence::Save);
     addAction(tr("Save as"), this, &ScoreMenu::onSaveAsActionTriggered, QKeySequence::SaveAs);
 #else
@@ -48,7 +48,7 @@ ScoreMenu::ScoreMenu(QWidget* parent)
     addAction(tr("Export to MIDI"), this, &ScoreMenu::onExportActionTriggered);
     addAction(tr("Export to Postscript"), this, &ScoreMenu::onExportPsActionTriggered);
     addAction(tr("Export to PDF"), this, &ScoreMenu::onExportPdfActionTriggered); //FIXME libspectre export fails.
-#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+#if (QT_VERSION < QT_VERSION_CHECK(6, 3, 0))
     addAction(tr("Close"), this, &ScoreMenu::onCloseActionTriggered, QKeySequence::Close);
     addAction(tr("Quit"), this, &ScoreMenu::onQuitActionTriggered, QKeySequence::Quit);
 #else
