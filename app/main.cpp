84373fc450b373f2ddcc0056f6fd5ee1101cdf1d
Benoît Rouits <brouits@free.fr>
2024-12-05 19:11:42 +0100

    Try to load qm translations from in-program resources.

--- app/main.cpp
+++ app/main.cpp
@@ -51,9 +51,11 @@ int main(int argc, char** argv)
 		abcapplication.installTranslator(&qtTranslator);
 
 	QTranslator qabcTranslator;
-	bool loaded = qabcTranslator.load(TARGET "_" + locale, "locale");
+	bool loaded = qabcTranslator.load(TARGET "_" + locale, ":/i18n/locale"); // from in-program resource
 	if (!loaded)
-		loaded = qabcTranslator.load(TARGET "_" + locale, DATADIR "/" TARGET "/locale");
+		loaded = qabcTranslator.load(TARGET "_" + locale, "locale"); // from ./
+	if (!loaded)
+		loaded = qabcTranslator.load(TARGET "_" + locale, DATADIR "/" TARGET "/locale"); // from installed data dir
 
 	if (loaded) {
 		abcapplication.installTranslator(&qabcTranslator);
